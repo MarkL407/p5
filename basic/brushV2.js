bfd554ce395cdd7b6ab16e70155cb8b938254b31
Marcu Leancă <marcuceva@protonmail.com>
2021-06-22 15:36:02 +0300

    Added erase button and draw when mouse is clicked
    i learned the mouseIsPressed var so i could do this, and I also added and erase button thanks to && which I also learned, thanks to The Coding Train

---
+++ basic/brushV2.js
@@ -0,0 +1,33 @@
+let r, g, b;
+let on = false;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(25);
+}
+
+function draw() {
+  noStroke();
+  fill(r, g, b);
+  r = map(mouseX, 0, 400, 0, 255);
+  b = map(mouseX, 0, 400, 255, 0);
+  g = map(mouseY, 0, 400, 0, 255);
+
+  if (mouseIsPressed) {
+    ellipse(mouseX, mouseY, 25);
+  }
+
+  if (mouseX > 0 && mouseX < 30 && mouseY > 0 && mouseY < 30) {
+    fill(255, 10, 10);
+
+    if (mouseIsPressed) {
+      background(25);
+    }
+  } else {
+    fill(230, 10, 10);
+  }
+
+  rect(0, 0, 30, 30, 10);
+
+  on = !on;
+}
